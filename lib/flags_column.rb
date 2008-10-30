8ea2cf321ca02c3f0ae1860d30e1a43511f2b73d
Martin Andert <mandert@gmx.net>
2008-10-30 23:48:07 +0100

    whitespace changes only

--- lib/flags_column.rb
+++ lib/flags_column.rb
@@ -2,85 +2,85 @@ module FlagsColumn
   def self.included(base)
     base.extend(ClassMethods)
   end
-  
+
   module ClassMethods
     def flags_column(attribute, flags, options = {})
       column = attribute.to_sym
-      
+
       unless respond_to?(:flagged_columns)
         write_inheritable_attribute :flagged_columns, {}
         class_inheritable_reader :flagged_columns
-        
+
         class_eval <<-EOV
           include FlagsColumn::InstanceMethods
-          
+
           def self.flagged_column_names
             read_inheritable_attribute(:flagged_columns).keys
           end
         EOV
-        
+
         alias_method_chain :method_missing, :flags
       end
-      
+
       unless instance_methods.include?('after_initialize')
         class_eval <<-EOV
           def after_initialize
           end
         EOV
       end
-      
+
       options_attribute = "#{column}_flags".to_sym
       write_inheritable_attribute(options_attribute, [])
       class_inheritable_reader options_attribute
-      
+
       bit_flags_attribute = "#{column}_bit_flags".to_sym
       write_inheritable_attribute(bit_flags_attribute, {})
       class_inheritable_reader bit_flags_attribute
-      
+
       bit_flags_inverted_attribute = "#{column}_bit_flags_inverted".to_sym
       write_inheritable_attribute(bit_flags_inverted_attribute, {})
       class_inheritable_reader bit_flags_inverted_attribute
-      
-      default_mask_attribute = "#{column}_default_mask".to_sym      
+
+      default_mask_attribute = "#{column}_default_mask".to_sym
       write_inheritable_attribute(default_mask_attribute, 0)
       class_inheritable_reader default_mask_attribute
-      
+
       class_eval <<-EOV
         def self.mask_#{column}(*flags)
           bit_flags = #{bit_flags_attribute}
-          
+
           flags.inject(0) do |n, name|
             n += bit_flags[name.to_sym]
           end
         end
-        
+
         def self.unmask_#{column}(mask)
           bit_flags_inverted = #{bit_flags_inverted_attribute}
-          
+
           bit_flags_inverted.inject([]) do |n, b|
             (mask & b.first) == b.first ? n.push(b.last) : n
           end
         end
       EOV
-      
+
       initial_flags = Array(options[:initial]).map(&:to_sym)
       read_inheritable_attribute(:flagged_columns)[column] = { :flags => flags, :initial => initial_flags }
-      
+
       flags.symbolize_keys!.each do |name, position|
         mask = (1 << position)
         read_inheritable_attribute(options_attribute) << name
         read_inheritable_attribute(bit_flags_attribute)[name] = mask
         read_inheritable_attribute(bit_flags_inverted_attribute)[mask] = name
-        
+
         if initial_flags.include?(name)
           write_inheritable_attribute(default_mask_attribute, read_inheritable_attribute(default_mask_attribute) + mask)
         end
-        
+
         define_method("#{column}_#{name}?".to_sym) do
           flagged_value = self[column] || 0
           flagged_value[position].eql? 1
         end
-          
+
         define_method("#{column}_#{name}".to_sym) do
           flagged_value = self[column] || 0
           flagged_value[position].eql? 1
@@ -92,14 +92,14 @@ module FlagsColumn
           self[column] = flag ? flagged_value |= mask : flagged_value &= ~mask
         end
       end
-      
+
       ["#{column}_none", "#{column}_none?"].each do |method|
         define_method(method.to_sym) do
           flagged_value = self[column] || 0
           flagged_value == 0
         end
       end
-      
+
       ["#{column}_all", "#{column}_all?"].each do |method|
         define_method(method.to_sym) do
           flagged_value = self[column] || 0
@@ -110,20 +110,20 @@ module FlagsColumn
       define_method("#{column}_flags".to_sym) do
         self.class.send("unmask_#{column}".to_sym, self[column] || 0)
       end
-          
+
       define_method("initialize_#{column}".to_sym) do
         self[column] = self.class.send("#{column}_default_mask".to_sym) if @new_record
       end
-      
+
       define_method("after_initialize_with_#{column}".to_sym) do
         send("after_initialize_without_#{column}".to_sym)
         send("initialize_#{column}".to_sym)
       end
-      
+
       alias_method_chain :after_initialize, column
     end
   end
-  
+
   module InstanceMethods
     def respond_to?(method, include_priv = false) #:nodoc:
       if method.to_s =~ /^(#{self.class.flagged_column_names.map(&:to_s).join('|')})_[^_]+_and_[^_]+/
@@ -134,23 +134,23 @@ module FlagsColumn
     end
 
     protected
-      
-      def method_missing_with_flags(method, *args, &block) #:nodoc:        
+
+      def method_missing_with_flags(method, *args, &block) #:nodoc:
         unless column = self.class.flagged_column_names.find { |name| method.to_s =~ /^#{name}_[^_]+_and_[^_]+/ }
           return method_missing_without_flags(method, *args, &block)
         end
-        
+
         options = self.class.flagged_columns[column]
-        
+
         if method.to_s =~ /^#{column}_([a-z0-9_]+)(\?|\=)?$/
           anded_flags, suffix = $1, $2
           flags = anded_flags.split('_and_').map(&:to_sym)
-          
+
           method_missing_without_flags(method, *args, &block) unless (flags - options[:flags].keys).empty?
-          
+
           flagged_value = self[column] || 0
           mask = self.class.send("mask_#{column}".to_sym, *flags)
-          
+
           if suffix == '='
             flag = ['true', '1', 'yes', 'ok'].include?(args.first.to_s.downcase)
             self[column] = flag ? flagged_value |= mask : flagged_value &= ~mask
@@ -161,6 +161,7 @@ module FlagsColumn
           method_missing_without_flags(method, *args, &block)
         end
       end
-            
+
   end
 end
+
--- test/flags_column_test.rb
+++ test/flags_column_test.rb
@@ -19,9 +19,9 @@ class Test::Unit::TestCase
       instance.id = attributes[:id] if attributes[:id] #the id attributes works differently on active record classes
       instance
     end
-      
+
     protected
-    
+
       def columns(attributes)
         attributes.keys.collect { |attribute| column attribute.to_s, attributes[attribute] }
       end
@@ -29,7 +29,7 @@ class Test::Unit::TestCase
       def column(column_name, value)
         ActiveRecord::ConnectionAdapters::Column.new(column_name, nil, ActiveRecordUnitTestHelper.active_record_type(value.class), false)
       end
-    
+
       def self.active_record_type(klass)
         return case klass.name
           when "Fixnum"         then "integer"
@@ -49,7 +49,7 @@ class Test::Unit::TestCase
   def assert_same_array_elements(left, right, msg = nil)
     assert (left - right).empty? && (right - left).empty?, msg
   end
-  
+
   def choose(n, k)
     return [[]] if n.nil? || n.empty? && k == 0
     return [] if n.nil? || n.empty? && k > 0
@@ -68,10 +68,10 @@ end
 
 # our test model
 class Model < ActiveRecord::Base
-  flags_column :visible_to, 
-               { :admins => 0, :members => 1, :friends => 2 }, 
+  flags_column :visible_to,
+               { :admins => 0, :members => 1, :friends => 2 },
                :initial => [:members, :friends]
-               
+
   flags_column :notify_when,
                { :created => 2, :updated => 4, :deleted => 7, :purged => 13 }
 end
@@ -80,186 +80,186 @@ class FlagsColumnTest < Test::Unit::TestCase # ActiveSupport::TestCase
   def test_class_should_respond_to_flagged_columns
     assert Model.respond_to?(:flagged_columns)
   end
-  
+
   def test_class_should_respond_to_flagged_column_names
     assert Model.respond_to?(:flagged_column_names)
   end
-  
+
   def test_flagged_columns_should_have_the_expected_keys
     assert_same_array_elements [:visible_to, :notify_when], Model.flagged_columns.keys
   end
-  
+
   def test_flagged_columns_keys_should_equal_flagged_column_names
     assert_same_array_elements Model.flagged_columns.keys, Model.flagged_column_names
   end
-  
+
   def test_flagged_column_flags_should_have_expected_flags
     expected = { :visible_to => [:admins, :members, :friends], :notify_when => [:created, :updated, :deleted, :purged] }
-    
+
     Model.flagged_columns.each { |name, options| assert_same_array_elements(options[:flags].keys, expected[name]) }
   end
-  
+
   def test_flagged_column_flags_should_have_expected_initials
     expected = { :visible_to => [:members, :friends], :notify_when => [] }
-    
+
     Model.flagged_columns.each { |name, options| assert_same_array_elements(options[:initial], expected[name]) }
   end
-  
+
   def test_class_should_respond_to_flags_column_name_plus_flags
     assert Model.flagged_column_names.all? { |name| Model.respond_to?("#{name}_flags".to_sym) }
   end
-  
+
   def test_class_should_respond_to_flags_column_name_plus_bit_flags
     assert Model.flagged_column_names.all? { |name| Model.respond_to?("#{name}_bit_flags".to_sym) }
   end
-  
+
   def test_class_should_respond_to_flags_column_name_plus_default_mask
     assert Model.flagged_column_names.all? { |name| Model.respond_to?("#{name}_default_mask".to_sym) }
   end
-  
+
   def test_class_should_respond_to_mask_plus_flags_column_name
     assert Model.flagged_column_names.all? { |name| Model.respond_to?("mask_#{name}".to_sym) }
   end
-  
+
   def test_class_should_respond_to_unmask_plus_flags_column_name
     assert Model.flagged_column_names.all? { |name| Model.respond_to?("unmask_#{name}".to_sym) }
   end
-  
+
   def test_instance_should_respond_to_flags_column_name_plus_flags
-    model = disconnected(Model).where(:visible_to => nil, :notify_when => nil)    
+    model = disconnected(Model).where(:visible_to => nil, :notify_when => nil)
     assert model.class.flagged_column_names.all? { |name| model.respond_to?("#{name}_flags".to_sym) }
   end
-  
+
   def test_instance_should_respond_to_flags_column_name_plus_all
-    model = disconnected(Model).where(:visible_to => nil, :notify_when => nil)    
+    model = disconnected(Model).where(:visible_to => nil, :notify_when => nil)
     assert model.class.flagged_column_names.all? { |name| model.respond_to?("#{name}_all".to_sym) && model.respond_to?("#{name}_all?".to_sym) }
   end
-  
+
   def test_instance_should_respond_to_flags_column_name_plus_none
-    model = disconnected(Model).where(:visible_to => nil, :notify_when => nil)    
+    model = disconnected(Model).where(:visible_to => nil, :notify_when => nil)
     assert model.class.flagged_column_names.all? { |name| model.respond_to?("#{name}_none".to_sym) && model.respond_to?("#{name}_none?".to_sym) }
   end
-  
+
   def test_instance_should_respond_to_flags_column_name_plus_flag_getters_and_setters
-    model = disconnected(Model).where(:visible_to => nil, :notify_when => nil)    
+    model = disconnected(Model).where(:visible_to => nil, :notify_when => nil)
     assert model.class.flagged_columns.all? { |name, options| options[:flags].keys.all? { |flag| model.respond_to?("#{name}_#{flag}".to_sym) && model.respond_to?("#{name}_#{flag}?".to_sym) && model.respond_to?("#{name}_#{flag}=".to_sym) } }
   end
-  
+
   def test_instance_should_respond_to_flags_column_name_plus_anded_flags_getters_and_setters
     model = disconnected(Model).where(:visible_to => nil, :notify_when => nil)
-    
-    assert model.class.flagged_columns.all? { |name, options| 
+
+    assert model.class.flagged_columns.all? { |name, options|
       anded_flags = options[:flags].keys.map(&:to_s).join('_and_')
       model.respond_to?("#{name}_#{anded_flags}".to_sym) && model.respond_to?("#{name}_#{anded_flags}?".to_sym) && model.respond_to?("#{name}_#{anded_flags}=".to_sym)
     }
   end
-  
+
   def test_new_instance_should_have_the_right_initial_flags_set
-    model = disconnected(Model).where(:visible_to => nil, :notify_when => nil)    
+    model = disconnected(Model).where(:visible_to => nil, :notify_when => nil)
     model.class.flagged_columns.each { |name, options| assert_same_array_elements(model.class.flagged_columns[name][:initial], model.send("#{name}_flags".to_sym)) }
   end
-  
+
   def test_all_flags_set_to_false_should_all_getter_return_false
     model = disconnected(Model).where(:visible_to => nil, :notify_when => nil)
-    
+
     model.class.flagged_columns.each do |name, options|
       options[:flags].keys.each do |flag|
         model.send("#{name}_#{flag}=", false)
       end
-      
+
       assert !model.send("#{name}_all?")
     end
   end
-  
+
   def test_all_flags_set_to_false_should_none_getter_return_true
     model = disconnected(Model).where(:visible_to => nil, :notify_when => nil)
-    
+
     model.class.flagged_columns.each do |name, options|
       options[:flags].keys.each do |flag|
         model.send("#{name}_#{flag}=", false)
       end
-      
+
       assert model.send("#{name}_none?")
     end
   end
-  
+
   def test_all_flags_set_to_true_should_all_getter_return_true
     model = disconnected(Model).where(:visible_to => nil, :notify_when => nil)
-    
+
     model.class.flagged_columns.each do |name, options|
       options[:flags].keys.each do |flag|
         model.send("#{name}_#{flag}=", true)
       end
-      
+
       assert model.send("#{name}_all?")
     end
   end
-  
+
   def test_all_flags_set_to_true_should_none_getter_return_false
     model = disconnected(Model).where(:visible_to => nil, :notify_when => nil)
-    
+
     model.class.flagged_columns.each do |name, options|
       options[:flags].keys.each do |flag|
         model.send("#{name}_#{flag}=", true)
       end
-      
+
       assert !model.send("#{name}_none?")
     end
   end
-  
+
   def test_only_one_flag_set_to_true_should_all_getter_return_false
     model = disconnected(Model).where(:visible_to => nil, :notify_when => nil)
-    
+
     model.class.flagged_columns.each do |name, options|
       options[:flags].keys.each do |flag|
         model.send("#{name}_#{flag}=", false)
       end
-      
+
       model.send("#{name}_#{options[:flags].keys.first}=", true)
-      
+
       assert !model.send("#{name}_all?")
     end
   end
-  
+
   def test_only_one_flag_set_to_true_should_none_getter_return_false
     model = disconnected(Model).where(:visible_to => nil, :notify_when => nil)
-    
+
     model.class.flagged_columns.each do |name, options|
       options[:flags].keys.each do |flag|
         model.send("#{name}_#{flag}=", false)
       end
-      
+
       model.send("#{name}_#{options[:flags].keys.first}=", true)
-      
+
       assert !model.send("#{name}_none?")
     end
   end
-  
+
   def test_assigned_flags_should_equal_expected_flags
     model = disconnected(Model).where(:visible_to => nil, :notify_when => nil)
-    
+
     model.class.flagged_columns.each do |name, options|
       flags = options[:flags].keys
-      
+
       (1..flags.size).to_a.each do |k|
         combinations = choose(flags, k)
-        
+
         combinations.each do |combination|
           reset_all_flags_to_false(model)
-          
+
           combination.each do |flag|
             model.send("#{name}_#{flag}=", true)
           end
-          
+
           assert_same_array_elements combination, model.send("#{name}_flags".to_sym), combination.inspect + ' ' + model.send("#{name}_flags".to_sym).inspect
           assert model.send("#{name}_#{combination.map(&:to_s).join('_and_')}?".to_sym)
         end
       end
     end
   end
-  
+
   private
-  
+
     def reset_all_flags_to_false(model)
       model.class.flagged_columns.each do |name, options|
         options[:flags].keys.each do |flag|
@@ -268,3 +268,4 @@ class FlagsColumnTest < Test::Unit::TestCase # ActiveSupport::TestCase
       end
     end
 end
+
